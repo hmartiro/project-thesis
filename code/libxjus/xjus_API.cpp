abf8a2d15f26497485d29cdbf767c8132b0b9938
Hayk Martirosyan <hayk.mart@gmail.com>
2013-04-20 15:05:00 -0400

    made two devices instead of one in libxjus

--- code/libxjus/xjus_API.cpp
+++ code/libxjus/xjus_API.cpp
@@ -10,7 +10,8 @@ using namespace std;
 char deviceName[] = "EPOS2";
 char protocolStackName[] = "EPOS2_USB";
 char interfaceName[] = "USB";
-char portName[] = "USB0";
+char portName1[] = "USB0";
+char portName2[] = "USB1";
 
 // Protocol stack settings
 const unsigned long BAUDRATE = 1000000;
@@ -21,8 +22,18 @@ unsigned long errorCode;
 const unsigned long ERROR_INFO_LENGTH = 100;
 char errorInfo[ERROR_INFO_LENGTH];
 
-// Handle of the connected device
-void* device;
+// Handle of the connected devices
+void* device1;
+void* device2;
+
+void* device(unsigned int node) {
+	if ((node == 1) || (node == 3) || (node == 5))
+		return device1;
+	else if ((node == 2) || (node == 4) || (node == 6))
+		return device2;
+	else
+		return 0;
+}
 
 void printError() {
 	VCS_GetErrorInfo(errorCode, errorInfo, ERROR_INFO_LENGTH);
@@ -34,117 +45,122 @@ unsigned long getErrorCode() {
 	return errorCode;
 }
 
-int openDevice() {
+int openDevices() {
 	printf("Attempting connection to %s", deviceName);
-	printf(" at %s...\n", portName);
-	device = VCS_OpenDevice(deviceName, protocolStackName, interfaceName, portName, &errorCode);
+	device1 = VCS_OpenDevice(deviceName, protocolStackName, interfaceName, portName1, &errorCode);
 	printError();
-	printf("Connected Device: %p\n", device);
+	printf("Connected Device 1: %p\n", device1);
+	device2 = VCS_OpenDevice(deviceName, protocolStackName, interfaceName, portName2, &errorCode);
+	printError();
+	printf("Connected Device 2: %p\n", device1);
 
 	//printf("Protocol stack settings are ");
 	//printf("baudrate %lu and timeout %lu.\n", BAUDRATE, TIMEOUT);
-	VCS_SetProtocolStackSettings(device, BAUDRATE, TIMEOUT, &errorCode);
+	VCS_SetProtocolStackSettings(device1, BAUDRATE, TIMEOUT, &errorCode);
+	VCS_SetProtocolStackSettings(device2, BAUDRATE, TIMEOUT, &errorCode);
 	printError();
 
-	if (device) return 1;
+	if ((device1) && (device2)) return 1;
 	else return -1;
 }
 
 unsigned short getState(unsigned short node) {
 	unsigned short state;
-	VCS_GetState(device, node, &state, &errorCode);
+	VCS_GetState(device(node), node, &state, &errorCode);
 	return state;
 }
 
 void clearFault(unsigned short node) {
 	//printf("Clearing fault at node %d...\n", node);
-	VCS_ClearFault(device, node, &errorCode);
+	VCS_ClearFault(device(node), node, &errorCode);
 	printError();
 }
 
 void enable(unsigned short node) {
 	//printf("Enabling node %d...\n", node);
-	VCS_SetEnableState(device, node, &errorCode);
+	VCS_SetEnableState(device(node), node, &errorCode);
 	printError();
 }
 
 void disable(unsigned short node) {
 	//printf("Disabling node %d...\n", node);
-	VCS_SetDisableState(device, node, &errorCode);
+	VCS_SetDisableState(device(node), node, &errorCode);
 	printError();
 }
 
 void closeDevice() {
 	//printf("Closing connection to device.\n");
-	VCS_CloseDevice(device, &errorCode);
+	VCS_CloseDevice(device1, &errorCode);
+	printError();
+	VCS_CloseDevice(device2, &errorCode);
 	printError();
 }
 
 long getPosition(unsigned short node) {
 	long pos;
-	VCS_GetPositionIs(device, node, &pos, &errorCode);
+	VCS_GetPositionIs(device(node), node, &pos, &errorCode);
 	printError();
 	return pos;
 }
 
 unsigned long getMaxFollowingError(unsigned short node) {
 	unsigned long followingError;
-	VCS_GetMaxFollowingError(device, node, &followingError, &errorCode);
+	VCS_GetMaxFollowingError(device(node), node, &followingError, &errorCode);
 	return followingError;
 }
 
 void setMaxFollowingError(unsigned short node, unsigned long followingError) {
-	VCS_SetMaxFollowingError(device, node, followingError, &errorCode);
+	VCS_SetMaxFollowingError(device(node), node, followingError, &errorCode);
 }
 
 unsigned long getMaxVelocity(unsigned int node) {
 	unsigned long velocity;
-	VCS_GetMaxProfileVelocity(device, node, &velocity, &errorCode);
+	VCS_GetMaxProfileVelocity(device(node), node, &velocity, &errorCode);
 	return velocity;
 }
 
 void setMaxVelocity(unsigned int node, unsigned long velocity) {
-	VCS_SetMaxProfileVelocity(device, node, velocity, &errorCode);
+	VCS_SetMaxProfileVelocity(device(node), node, velocity, &errorCode);
 }
 
 unsigned long getMaxAcceleration(unsigned int node) {
 	unsigned long acceleration;
-	VCS_GetMaxAcceleration(device, node, &acceleration, &errorCode);
+	VCS_GetMaxAcceleration(device(node), node, &acceleration, &errorCode);
 	return acceleration;
 }
 
 void setMaxAcceleration(unsigned int node, unsigned long acceleration) {
-	VCS_SetMaxAcceleration(device, node, acceleration, &errorCode);
+	VCS_SetMaxAcceleration(device(node), node, acceleration, &errorCode);
 }
 
 void profilePositionMode(unsigned short node) {
 	//printf("Switching to profile position mode at node %d \n", node);
-	VCS_ActivateProfilePositionMode(device, node, &errorCode);
+	VCS_ActivateProfilePositionMode(device(node), node, &errorCode);
 	printError();
 }
 
 void moveRelative(unsigned short node, long pos) {
 	profilePositionMode(node);
-	VCS_MoveToPosition(device, node, pos, false, false, &errorCode);
+	VCS_MoveToPosition(device(node), node, pos, false, false, &errorCode);
 	printError();
 }
 
 void moveAbsolute(unsigned short node, long pos) {
 	profilePositionMode(node);
-	VCS_MoveToPosition(device, node, pos, true, false, &errorCode);
+	VCS_MoveToPosition(device(node), node, pos, true, false, &errorCode);
 	printError();
 }
 
 void interpolationMode(unsigned short node) {
 	//printf("Switching to IPM mode at node %d \n", node);
-	VCS_ActivateInterpolatedPositionMode(device, node, &errorCode);
+	VCS_ActivateInterpolatedPositionMode(device(node), node, &errorCode);
 	printError();
-	VCS_ClearIpmBuffer(device, node, &errorCode);
+	VCS_ClearIpmBuffer(device(node), node, &errorCode);
 	printError();
 }
 
 void addPVT(unsigned short node, long p, long v, unsigned int t) {
-	VCS_AddPvtValueToIpmBuffer(device, node, p, v, t, &errorCode);
+	VCS_AddPvtValueToIpmBuffer(device(node), node, p, v, t, &errorCode);
 	printError();
 }
 
@@ -156,62 +172,62 @@ void addPvtAll(int N, unsigned short node[], long p[], long v[], unsigned char t
 
 	for(int i = 0; i < N; i++) {
 		//printf("node: %u, p: %ld, v: %ld, t: %u \n", node[i], p[i], v[i], t[i]);
-		VCS_AddPvtValueToIpmBuffer(device, node[i], p[i], v[i], t[i], &errorCode);
+		VCS_AddPvtValueToIpmBuffer(device(node[i]), node[i], p[i], v[i], t[i], &errorCode);
 	}
 	printError();
 }
 
 void startIPM(unsigned short node) {
 	//printf("Starting IPM trajectory at node %d \n", node);
-	VCS_StartIpmTrajectory(device, node, &errorCode);
+	VCS_StartIpmTrajectory(device(node), node, &errorCode);
 	printError();
 }
 
 void stopIPM(unsigned short node) {
 	//printf("Stopping IPM trajectory at node %d \n", node);
-	VCS_StopIpmTrajectory(device, node, &errorCode);
+	VCS_StopIpmTrajectory(device(node), node, &errorCode);
 	printError();
 }
 
 long getFreeBufferSize(unsigned short node) {
 	unsigned long bufferSize;
-	VCS_GetFreeIpmBufferSize(device, node, &bufferSize, &errorCode);
+	VCS_GetFreeIpmBufferSize(device(node), node, &bufferSize, &errorCode);
 	//printError();
 	return bufferSize;
 }
 
 void clearIpmBuffer(unsigned short node) {
 	interpolationMode(node);
-	VCS_ClearIpmBuffer(device, node, &errorCode);
+	VCS_ClearIpmBuffer(device(node), node, &errorCode);
 	printError();
 }
 
 void homingMode(unsigned short node) {
 	//printf("Switching to homing position mode at node %d \n", node);
-	VCS_ActivateHomingMode(device, node, &errorCode);
+	VCS_ActivateHomingMode(device(node), node, &errorCode);
 	printError();
 }
 
 void zeroPosition(unsigned short node) {
 	//printf("Setting current position to zero at node %d \n", node);
-	VCS_DefinePosition(device, node, 0, &errorCode);
+	VCS_DefinePosition(device(node), node, 0, &errorCode);
 	printError();
 }
 
 void setPositionProfile(unsigned short node, long vel, long accel, long deaccel) {
 	//printf("Setting position profile for node %d \n", node);
-	VCS_SetPositionProfile(device, node, vel, accel, deaccel, &errorCode);
+	VCS_SetPositionProfile(device(node), node, vel, accel, deaccel, &errorCode);
 	printError();
 }
 
 void getPositionRegulatorGain(unsigned short node) {
 	unsigned short pP, pI, pD;
-	VCS_GetPositionRegulatorGain(device, node, &pP, &pI, &pD, &errorCode);
+	VCS_GetPositionRegulatorGain(device(node), node, &pP, &pI, &pD, &errorCode);
     //printf("node: %d, pP: %d, pI: %d, pD: %d \n", node, pP, pI, pD);
 }
 
 void setPositionRegulatorGain(unsigned short node, unsigned short P, unsigned short I, unsigned short D) {
-	VCS_SetPositionRegulatorGain(device, node, P, I, D, &errorCode);
+	VCS_SetPositionRegulatorGain(device(node), node, P, I, D, &errorCode);
 	printError();
 }
 
@@ -222,7 +238,7 @@ signed short getNodeAvgCurrent(unsigned short node){
 	unsigned short currentObject = 8231; //WORD, uINT16
 	unsigned long pNbOfBytesRead = 2;    //DWORD, uINT32
 
-	VCS_GetObject(device, node, currentObject, 0, &nodeCurrent, 2, &pNbOfBytesRead, &errorCode );
+	VCS_GetObject(device(node), node, currentObject, 0, &nodeCurrent, 2, &pNbOfBytesRead, &errorCode );
 
 	// Take the absolute value of the current, ignoring regenerative effects
 	if (nodeCurrent < 0)
@@ -248,7 +264,7 @@ void printIpmStatus(unsigned short node) {
 	int velocityError;
 	int accelerationError;
 
-	VCS_GetIpmStatus(device, node, &trajectoryRunning, &underflowWarning, &overflowWarning,
+	VCS_GetIpmStatus(device(node), node, &trajectoryRunning, &underflowWarning, &overflowWarning,
 			&velocityWarning, &accelerationWarning, &underflowError, &overflowError,
 			&velocityError, &accelerationError, &errorCode);
 
@@ -266,7 +282,7 @@ void printIpmStatus(unsigned short node) {
 
 int isFinished(unsigned short node) {
 	int targetReached;
-	VCS_GetMovementState(device, node, &targetReached, &errorCode);
+	VCS_GetMovementState(device(node), node, &targetReached, &errorCode);
 	printError();
 	return targetReached;
 }
